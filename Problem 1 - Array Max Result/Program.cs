9b1e8aa970db2a89c270668a6cdd395bb437a705
racarter1215 <racarter1215@gmail.com>
2020-07-05 16:21:20 -0700

    code challenge 1 done

---
+++ Problem 1 - Array Max Result/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Problem_1___Array_Max_Result
+{
+    class Program
+    {
+        public static void Test1()
+        {
+            int[] numbers = new int[5];
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Select 5 Numbers between 1 and 10");
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
+            Console.Write(@"You have selected {0}, {1}, {2}, {3}, and {4}. Please select a number to recieve your score! ", numbers[0], numbers[1], numbers[2], numbers[3], numbers[4]);
+            int selection = int.Parse(Console.ReadLine());
+            int score = selection * Array.FindAll(numbers, x => x == selection).Length;
+            Console.Write($"Your score is {score}!");
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Test1();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # 401-prework-code-challenge
+### The Array Max Result Challenge
+### One needs to create a program that allows a user to select 5 numbers between 1 and 10 and return a score for the selected number
+
+![Test Image 1](401-screenshots/code-challenge-1.png)
