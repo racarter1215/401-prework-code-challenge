f3ce2e006a9d2dbcf9898e809c4d7d9c9d8221b0
racarter1215 <racarter1215@gmail.com>
2020-07-05 17:12:16 -0700

    got challenge 2 done, mostly done 3 and 4

---
+++ Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace Problem_1___Array_Max_Result
+{
+    class Program
+    {
+        public static void Test1()
+        {
+            int[] numbers = new int[5];
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Select 5 Numbers between 1 and 10");
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
+            Console.Write(@"You have selected {0}, {1}, {2}, {3}, and {4}. Please select a number to recieve your score! ", numbers[0], numbers[1], numbers[2], numbers[3], numbers[4]);
+            int selection = int.Parse(Console.ReadLine());
+            int score = selection * Array.FindAll(numbers, x => x == selection).Length;
+            Console.Write($"Your score is {score}!");
+        }
+
+        public static void Test2()
+        {
+            Console.WriteLine("Write down a year");
+            int year = int.Parse(Console.ReadLine());
+
+            if (year % 400 == 0)
+            {
+                Console.WriteLine($"The year {year} is a leap year");
+            }
+            else if (year % 100 == 0)
+            {
+                Console.WriteLine($"The year {year} is not a leap year");
+            }
+            else if (year % 100 == 0)
+            {
+                Console.WriteLine($"The year {year} is a leap year");
+            }
+            else
+            {
+                Console.WriteLine($"The year {year} is not a leap year");
+            }
+        }
+
+        public static void Test3()
+        {
+            int[] arr = new int[] { 1, 2, 3 };
+            if (arr[0] + arr[1] + arr[2] == 6 && arr[0] * arr[1] * arr[2] == 6)
+            {
+                Console.WriteLine("This is a perfect array");
+            }
+            else
+            {
+                Console.WriteLine("This is not a perfect array");
+            }
+        }
+
+        public static int[] Test4(int[,] numList)
+        {
+            int[] myArray = new int[numList.GetLength(0)];
+            for (int i = 0; i < numList.GetLength(0); i++)
+            {
+                for (int j = 0; j < numList.GetLength(1); j++)
+                {
+                    myArray[i] += numList[i, j];
+                }
+            }
+            foreach(int num in myArray)
+            {
+                Console.WriteLine($"{num}");
+            }
+            Console.Write("\n");
+            return myArray;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Test1();
+            Test2();
+            Test3();
+            Test4();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -3,3 +3,21 @@
 ### One needs to create a program that allows a user to select 5 numbers between 1 and 10 and return a score for the selected number
 
 ![Test Image 1](401-screenshots/code-challenge-1.png)
+
+# 401-prework-code-challenge
+### Leap Year Challenge
+### One needs to create a program that allows a user to verify if a year is a leap year
+
+![Test Image 2](401-screenshots/code-challenge-2.png)
+
+# 401-prework-code-challenge
+### The Array Max Result Challenge
+### One needs to create a program that allows a user to select 5 numbers between 1 and 10 and return a score for the selected number
+
+![Test Image 3](401-screenshots/code-challenge-3.png)
+
+# 401-prework-code-challenge
+### The Array Max Result Challenge
+### One needs to create a program that allows a user to select 5 numbers between 1 and 10 and return a score for the selected number
+
+![Test Image 4](401-screenshots/code-challenge-4.png)
